4aa39e3f2b635a31886bddcf3f259d4b8fcad579
Michael Peeters <mich.peeters@gmail.com>
2020-09-02 12:23:16 +0200

    cronscripts nightly and hourly

---
+++ pvoutput_hourly.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LOGFILE=/tmp/sunnyportal2pvoutput.hourly.log
+
+#rotate logs
+for i in {9..1}; do
+    if [[ -f $LOGFILE.${i} ]]; then
+        mv -f $LOGFILE.${i} $LOGFILE.$((i+1))
+    fi
+done
+mv -f $LOGFILE $LOGFILE.1
+
+exec >> $LOGFILE
+exec 2>&1
+
+cd "$(dirname "$0")"
+
+#hardkill after 5min
+PYTHONPATH=. timeout -s 9 300 ./bin/sunnyportal2pvoutput --status sunnyportal.config
+
+cd -
